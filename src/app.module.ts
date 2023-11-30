4e7aa3ada29d925479aea9f93cc65088f9aa5443
lae1965 <lae1965@yandex.ru>
2023-11-30 12:59:39 +0300

    Change README.md, add path to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,14 @@ import { ContactsModule } from './contacts/contacts.module';
 import { LeadsModule } from './leads/leads.module';
 import { AuthModule } from './auth/auth.module';
 import { ApiModule } from './api/api.module';
+import * as path from 'path';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(__dirname, '..', '.env'),
+    }),
     ContactsModule,
     LeadsModule,
     AuthModule,
